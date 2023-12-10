16c1e649ffa838b8e6220b7dee8c83272bee2083
Jessy Gjerek <2232678@champlaincollege.qc.ca>
2023-12-10 15:55:26 -0500

    First Changes

---
+++ ForTheProject/HandlerPhoneNumber.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTheProject
+{
+    internal class HandlerPhoneNumber
+    {
+        static readonly HandlerPhoneNumber instance = new HandlerPhoneNumber();
+
+        static readonly string Constring = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+
+        private HandlerPhoneNumber()
+        {
+            CreateTable();
+
+            PhoneNumber newP1 = new PhoneNumber()
+            {
+                Number = "450-444-2312",
+                Type = "Work",
+
+
+
+
+            };
+            PhoneNumber newP2 = new PhoneNumber()
+            {
+                Number = "450-312-444",
+                Type = "Home",
+
+
+
+            };
+
+
+
+            //seed the table
+            AddPerson(newP1);
+            AddPerson(newP2);
+        }
+
+        public static HandlerPhoneNumber Instance
+        {
+            get { return instance; }
+        }
+
+        public void CreateTable()
+        {
+            using (SQLiteConnection con = new SQLiteConnection(Constring))
+
+
+            {
+                con.Open();
+                string drop = "drop table if exists PhoneNumber;";
+                SQLiteCommand command1 = new SQLiteCommand(drop, con);
+                command1.ExecuteNonQuery();
+
+                string table = "create table PERSONS (PhoneNumberId integer primary key, Number text, Type text);";
+                SQLiteCommand command2 = new SQLiteCommand(table, con);
+                command2.ExecuteNonQuery();
+            }
+        }
+
+        public int AddPerson(PhoneNumber phoneNumber)
+        {
+            int rows = 0;
+            int newId = 0;
+            using (SQLiteConnection con = new SQLiteConnection(Constring))
+            {
+
+                con.Open();
+                string query = "INSERT INTO PERSONS (Number,Type) VALUES (@Number, @Type)";
+                SQLiteCommand insertcom = new SQLiteCommand(query, con);
+
+                insertcom.Parameters.AddWithValue("@Number", phoneNumber.Number);
+                insertcom.Parameters.AddWithValue("@Type", phoneNumber.Type);
+
+
+                try
+                {
+                    rows = insertcom.ExecuteNonQuery();
+                    //lets get the rowid inserted
+                    insertcom.CommandText = "select last_insert_rowid()";
+                    Int64 LastRowID64 = Convert.ToInt64(insertcom.ExecuteScalar());
+                    //then grab the bottom 32 bits as the unique id of the row
+                    newId = Convert.ToInt32(LastRowID64);
+
+                }
+                catch (SQLiteException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            return newId;
+        }
+        }
+}
---
+++ ForTheProject/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTheProject
+{
+    class PhoneNumber
+    {
+        public int PhoneNumberId { get; set; }
+        public string Number { get; set; }
+        public string Type { get; set; } // Home, Work, Mobile, etc.
+        public int ResumeId { get; set; } // Foreign key referencing the main resume table
+
+
+
+        public override string ToString()
+        {
+            string formattted = String.Format("{0}\t {1}\t {2}\t| {3}\t {4}\t {5}", PhoneNumberId, Number, Type, ResumeId);
+            return formattted;
+        }
+    }
+}
